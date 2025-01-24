027e09a1552a51e39e051408f9405da2dac45973
Nick <masihnishit@gmail.com>
2025-01-25 00:23:38 +0530

    hello world controller

---
+++ src/main/java/com/alpha/www/RestApis/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.alpha.www.RestApis.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello-World!";
+	}
+}
