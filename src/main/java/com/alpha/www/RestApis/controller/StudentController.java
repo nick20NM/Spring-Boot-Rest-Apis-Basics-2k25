616dedf34bb88034aafce0ae81945dbc9ecd4971
Nick <masihnishit@gmail.com>
2025-01-25 00:54:24 +0530

    student list rest api

--- src/main/java/com/alpha/www/RestApis/controller/StudentController.java
+++ src/main/java/com/alpha/www/RestApis/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.alpha.www.RestApis.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,15 @@ public class StudentController {
 	@GetMapping("/student")
 	public Student getStudent() {
 		return new Student(1, "tom", "sharma");
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		List<Student> students = new ArrayList<>();
+		students.add(new Student(1, "tom", "sharma"));
+		students.add(new Student(2, "jerry", "agrawal"));
+		students.add(new Student(3, "john", "cena"));
+		students.add(new Student(4, "abc", "xyz"));
+		return students;
 	}
 }
