3e97c8edef5331189956e4113cb8e234eb0f9ee0
NM <masihnishit@gmail.com>
2025-01-25 00:09:24 +0530

    new spring boot project

---
+++ src/main/java/com/alpha/www/RestApis/SpringBootRestApisBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.www.RestApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApisBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApisBasicsApplication.class, args);
+	}
+
+}
