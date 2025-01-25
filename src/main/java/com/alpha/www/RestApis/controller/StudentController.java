fdbe1850a6992e509389e3e7c4ee378b34d29dc2
Nick <masihnishit@gmail.com>
2025-01-25 11:34:39 +0530

    student rest api with query parameters

--- src/main/java/com/alpha/www/RestApis/controller/StudentController.java
+++ src/main/java/com/alpha/www/RestApis/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alpha.www.RestApis.bean.Student;
@@ -33,5 +34,14 @@ public class StudentController {
 			@PathVariable("first-name") String firstName, 
 			@PathVariable("last-name") String lastName) {
 		return new Student(studentId, firstName, lastName);
+	}
+	
+	// localhost:8080/student/query?id=1&firstName=tom&lastName=sharma
+	@GetMapping("/student/query")
+	public Student studentQueryParameter(
+			@RequestParam int id, 
+			@RequestParam String firstName,
+			@RequestParam String lastName) {
+		return new Student(id, firstName, lastName);
 	}
 }
